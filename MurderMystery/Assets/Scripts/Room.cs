9f2e8a466c0d1c51e7a9c4876159e075f3f01cd1
mcosta1 <mcosta1@uncc.edu>
2016-03-01 16:33:08 -0500

    Mystery

---
+++ MurderMystery/Assets/Scripts/Room.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room : MonoBehaviour {
+    private List<Interaction> available;
+    private List<Room> connections;
+	// Use this for initialization
+	void Start () {
+        available = new List<Interaction>();
+        connections = new List<Room>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void addRoom(Room m)
+    {
+        connections.Add(m);
+    }
+    public void addobj(Interaction m)
+    {
+        available.Add(m);
+    }
+}
