b8b60b4376f4ab11b07c65d5c43c34c2aac90709
Kirsti <camlin@carolina.rr.com>
2016-04-27 21:50:40 -0400

    Buttons and beginning Stats (not sure if i'm doing it right tho)

--- MurderMystery/Assets/Scripts/Shop.cs
+++ MurderMystery/Assets/Scripts/Shop.cs
@@ -6,7 +6,8 @@ public class Shop : MonoBehaviour {
     List<House_feature> availableItems;
     House_feature[] aval;
     Parser p = new Parser();
-	// Use this for initialization
+	
+    // Use this for initialization
 	void Start () {
         availableItems = p.getShop("Feature_List.muda");
         aval = availableItems.ToArray();
@@ -17,24 +18,94 @@ public class Shop : MonoBehaviour {
 	void Update () {
 	
 	}
-    public void purchaseItem(Business ok,int ID)
+    
+    //Zombie
+    public void purchaseZombie(Business ok)
     {
-        if (ok.Money1 > aval[ID].purchase())
+        if (ok.Money1 > aval[0].purchase())
         {
-            ok.makePurchase(aval[ID]);
+            ok.makePurchase(aval[0]);
         }
     }
-    /*
-        IDList
-        0.Zombie
-        1.Jack In the box
-        2.Minor ghost
-        3.Witch
-        4.Vamp
-        5.Werewolf
-        6.Minor Black Cat
-        7.Minor Spider
-        8.Cobwebs
-        9.THIS
-    */
+
+    //Jack
+    public void purchaseJack(Business ok)
+    {
+        if (ok.Money1 > aval[1].purchase())
+        {
+            ok.makePurchase(aval[1]);
+        }
+    }
+
+    //Ghost
+    public void purchaseGhost(Business ok)
+    {
+        if (ok.Money1 > aval[2].purchase())
+        {
+            ok.makePurchase(aval[2]);
+        }
+    }
+
+    //Witch
+    public void purchaseWitch(Business ok)
+    {
+        if (ok.Money1 > aval[3].purchase())
+        {
+            ok.makePurchase(aval[3]);
+        }
+    }
+
+    //Vamp
+    public void purchaseVamp(Business ok)
+    {
+        if (ok.Money1 > aval[4].purchase())
+        {
+            ok.makePurchase(aval[4]);
+        }
+    }
+
+    //Wolf
+    public void purchaseWolf(Business ok)
+    {
+        if (ok.Money1 > aval[5].purchase())
+        {
+            ok.makePurchase(aval[5]);
+        }
+    }
+
+    //Cat
+    public void purchaseCat(Business ok)
+    {
+        if (ok.Money1 > aval[6].purchase())
+        {
+            ok.makePurchase(aval[6]);
+        }
+    }
+
+    //Spider
+    public void purchaseSpider(Business ok)
+    {
+        if (ok.Money1 > aval[7].purchase())
+        {
+            ok.makePurchase(aval[7]);
+        }
+    }
+
+    //Web
+    public void purchaseWeb(Business ok)
+    {
+        if (ok.Money1 > aval[8].purchase())
+        {
+            ok.makePurchase(aval[8]);
+        }
+    }
+
+    //THIS
+    public void purchaseTHIS(Business ok)
+    {
+        if (ok.Money1 > aval[9].purchase())
+        {
+            ok.makePurchase(aval[9]);
+        }
+    }
 }
